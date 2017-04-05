9b13ddf561739baf162fa03065e136524bc7437e
PedramPedram <pedram_65@yahoo.com>
2017-04-05 12:49:41 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,62 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#makeVector <- function(x = numeric()) {
+#  m <- NULL
+#  set <- function(y) {
+#    x <<- y
+#    m <<- NULL
+#  }
+#  get <- function() x
+#  setmean <- function(mean) m <<- mean
+#  getmean <- function() m
+#  list(set = set, get = get,
+#       setmean = setmean,
+#       getmean = getmean)
+#}
 
+makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y) {
+          x <<- y
+          m <<- NULL
+      }
+      get <- function() x
+      setinve <- function(solve) m <<- solve
+      getinve <- function() m
+      list(set = set, get = get,
+           setinve = setinve,
+           getinve = getinve)
 }
 
 
 ## Write a short comment describing this function
 
+#cachemean <- function(x, ...) {
+#  m <- x$getmean()
+#  if(!is.null(m)) {
+#    message("getting cached data")
+#    return(m)
+#  }
+#  data <- x$get()
+#  m <- mean(data, ...)
+#  x$setmean(m)
+#  m
+#}
+
+
+
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getinve()
+      if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinve(m)
+      m
 }
+
+
